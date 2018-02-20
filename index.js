d5da591e5f3c3a7677c644e3d2200451c6daddbb
shashank-dewangan <shashank.at30@gmail.com>
2018-02-20 16:53:53 +0530

    added health router

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ var app = express();
 
 var port = process.env.PORT || 4000;
 app.listen(port);
+
 app.get('/',function(req,res){
-res.status(400);
-res.send("Hello World");
-})
+    res.status(400);
+    res.send("Hello World");
+});
+
+app.get('/health',function(req,res){
+    res.status(400);
+    res.send("running successfully")
+});
